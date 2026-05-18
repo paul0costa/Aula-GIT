057aa500cffc89c78cd782fc6621a12d517786ff
Paulo <paulorscosta@live.com>
2026-05-18 23:46:19 +0800

    Initial Commit

---
+++ teste ebac/teste_ebacApp.swift
@@ -0,0 +1,17 @@
+//
+//  teste_ebacApp.swift
+//  teste ebac
+//
+//  Created by Paulo Costa on 5/18/26.
+//
+
+import SwiftUI
+
+@main
+struct teste_ebacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
